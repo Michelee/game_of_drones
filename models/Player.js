00fd28fddf7e300e2ff89e10ad664aa8b014e8c3
Michele Ancheta <michelee.ancheta@gmail.com>
2019-05-18 18:59:35 -0500

    add endpoint to update game

--- models/Player.js
+++ models/Player.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const playerSchema = new Schema({
-    name: String
+    name: String,
+    victories: {
+        type: Number,
+        default: 0
+    }
 });
 
 mongoose.model('players', playerSchema);
--- routes/gameRoutes.js
+++ routes/gameRoutes.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const handlePlayer = require('../services/handlePlayer');
+
 const Game = mongoose.model('games');
+const Player = mongoose.model('players');
 
 module.exports = app => {
     app.post('/api/newgame', async (req, res) => {
@@ -9,7 +11,6 @@ module.exports = app => {
         if (!namePlayerOne || !namePlayerTwo) {
             res.status(400).send("Missing information");
         } else {
-
             try {
                 playerOne = await handlePlayer(namePlayerOne);
                 playerTwo = await handlePlayer(namePlayerTwo);
@@ -28,6 +29,21 @@ module.exports = app => {
             } catch (error) {
                 res.status(500).send(error);
             }
+        }
+    });
+
+    app.post('/api/endgame', async (req, res) => {
+        const { winnerId, gameId } = req.body;
+
+        if (!winnerId || !gameId) {
+            res.status(400).send("Missing information");
+        } else {
+            const player = await Player.findOne({_id: winnerId});
+            player.victories += 1;
+            player.save();
+            
+            Game.updateOne({ _id: gameId }, { $set: { '_winner': player._id }}).exec();
+            res.send({text: 'Successfully saved'});
         }
     });
 };
