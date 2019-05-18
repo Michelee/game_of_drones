0843feb4745ada83f1cd2193ae8ba035c5cadd15
Michele Ancheta <michelee.ancheta@gmail.com>
2019-05-18 15:57:20 -0500

    update end of game component

--- client/src/components/game/endOfGame.js
+++ client/src/components/game/endOfGame.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '../../styles/components/_endOfGame.scss'
 
 const EndOfGame = ({ winner }) => (
     <div className="EndOfGameComponent">
         <h1>We have a WINNER!!</h1>
-        <h3>{winner}is the new EMPEROR!</h3>
+        <h3>{winner} is the new EMPEROR!</h3>
+
+        <Link to="/" className="btn">
+            Play Again
+        </Link>
     </div>
 );
 
